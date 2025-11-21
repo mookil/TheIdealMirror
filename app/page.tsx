18aa2093e01766db29de23cd299460a45e5140db
mookil <mikael2aa@gmail.com>
2025-11-21 03:24:47 -0700

    prototype styling
    - separated main page into separate divs, Header, Team Loadout, Collection, and Footer
    
    - placeholder div boxes for now to match my figma sketches

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,109 @@
-import Image from "next/image";
+import React from 'react'
+// The main page of "TheIdealMirror"
 
-export default function Home() {
+
+
+function Home() {
+    const sinnerBox = "bg-amber-500 flex-1 m-1 pt-10 pb-10"
+
+    const collectionBox = "bg-amber-600 flex-1 m-1 pt-2 pb-2"
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="text-center">
+        {/* Header Area */}
+        <div className="bg-gray-900 rounded-2xl p-3">
+            <h1 className="text-3xl text-center font-bold">The Ideal Mirror</h1>
+            <h1 className="text-center">~ Limbus Company Tool ~</h1>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        {/* Team Loadout Area */}
+        <div className="bg-gray-900 rounded-2xl p-3 mt-10">
+            <h1 className="text-2xl text-center">Team Loadout</h1>
+            {/* Flex Box Container Temporary */}
+            <div className="flex">
+                <div className={sinnerBox}>Yi Sang</div> {/* This is a cube. */}
+                <div className={sinnerBox}>Faust</div> {/* This is a cube. */}
+                <div className={sinnerBox}>Don Quixote</div> {/* This is a cube. */}
+                <div className={sinnerBox}>Ryoshu</div> {/* This is a cube. */}
+                <div className={sinnerBox}>Meursault</div> {/* This is a cube. */}
+                <div className={sinnerBox}>Hong Lu</div> {/* This is a cube. */}
+            </div>
+            <div className="flex">
+                <div className={sinnerBox}>Heathcliff</div> {/* This is a cube. */}
+                <div className={sinnerBox}>Ishmael</div> {/* This is a cube. */}
+                <div className={sinnerBox}>Rodion</div> {/* This is a cube. */}
+                <div className={sinnerBox}>Sinclair</div> {/* This is a cube. */}
+                <div className={sinnerBox}>Outis</div> {/* This is a cube. */}
+                <div className={sinnerBox}>Gregor</div> {/* This is a cube. */}
+            </div>
+
+            {/* Affinities */}
+            <div>
+                <h1 className="underline text-2xl">Affinities</h1>
+                <p>Wrath: x2/5</p>
+                <p>Sloth: x6/8</p>
+                <p>Gluttony: x6/9</p>
+                <p>Cheese: x6/7</p>
+                <p>Gloom: x6/7</p>
+
+            </div>
+
         </div>
-      </main>
+
+        {/* Collection Area */}
+        <div className="bg-gray-900 rounded-2xl p-3 mt-10">
+            <h1 className="text-2xl text-center">Collection</h1>
+            <div className="flex">
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+            </div>
+            <div className="flex">
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+            </div>
+            <div className="flex">
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+                <div className={collectionBox}>1</div> {/* This is a cube. */}
+            </div>
+
+            
+        </div>
+
+        {/* Footer Area */}
+        <div className="bg-gray-900 rounded-2xl p-3 mt-10">
+            <h1 className="text-2xl text-center">About</h1>
+            <p>beepboopbeobrobepsborbserpsorbosopbsoer</p>
+            <p>beepboopbeobrobepsborbserpsorbosopbsoer</p>
+            <p>beepboopbeobrobepsborbserpsorbosopbsoer</p>
+            <p>beepboopbeobrobepsborbserpsorbosopbsoer</p>
+            <p>beepboopbeobrobepsborbserpsorbosopbsoer</p>
+
+        </div>
     </div>
-  );
+  )
 }
+
+export default Home
